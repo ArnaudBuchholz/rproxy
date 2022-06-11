d98d331b9b09cbed512ed1c8ae6586e22438a4ed
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-11 13:50:27 -0400

    Save logs

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
 'use strict'
 
 global.cfg = require('./.rproxy.json')
-const { log, check, serve } = require('reserve')
+const { check, serve } = require('reserve')
+const { writeFileSync } = require('fs')
+
+function log (...args) {
+  const now = new Date().toISOString()
+  const fileName = `logs/${now.substring(0, 10)}.csv`
+  writeFileSync(fileName, [now, ...args]
+    .map(arg => arg.toString())
+    .map(arg => arg.match(/"|;/) ? `"${arg.replace(/"|\//g, m => `\\${m}`)}"` : arg)
+    .join(';') + '\n',
+  {
+    flag: 'a+'
+  })
+}
+
 check({
   port: 80,
   mappings: [{
@@ -24,5 +38,18 @@ check({
     file: '403.html'
   }]
 }).then(configuration => {
-  log(serve(configuration), true)
+  serve(configuration)
+    .on('incoming', event => !event.internal ? log('INCMG', event.id, JSON.stringify({ ...event, id: undefined })) : 0)
+    .on('redirecting', ({ id, internal, type, redirect }) => {
+      if (!internal) {
+        if (typeof redirect === 'function') {
+          redirect = redirect.name || 'anonymous'
+        } else {
+          redirect = redirect.toString()
+        }
+        log('RDRCT', id, JSON.stringify({ type, redirect }))
+      }
+    })
+    .on('redirected', ({ id, internal, timeSpent, statusCode }) => !internal ? log('SERVE', id, JSON.stringify({ timeSpent, statusCode })) : 0)
+    .on('error', ({ id, internal, error }) => !internal ? log('ERROR', id, error.toString()) : 0)
 })
