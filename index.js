5dd119f8b78a31cc4c688661563d29f2d1f6ba8f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-11 01:16:05 -0400

    wip

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-require('dotenv').config()
+#!/usr/bin/env node
+
+'use strict'
+
+global.cfg = require('./.rproxy.json')
+const jose = require('jose')
 const { log, check, serve } = require('reserve')
 check({
   port: 80,
   mappings: [{
+    custom: require('./is-authenticated.js')
+  }, {
     method: 'GET',
-    match: '/login$',
+    match: '/login',
     file: 'login.html'
   }, {
     file: '403.html'
