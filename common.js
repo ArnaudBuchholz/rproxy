994eab9d6d5f097e1f1184ca5cff6a1d702ab96f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-11 12:44:05 -0400

    Common helpers

---
+++ common.js
@@ -0,0 +1,43 @@
+const cookieNames = {
+  jwt: 'jwt',
+  redirect: 'redirect'
+}
+
+const cookies = (request, name) => {
+  const cookiesStr = request.headers.cookie
+  if (cookiesStr) {
+    return cookiesStr
+      .split(';')
+      .map(cookie => cookie.trim())
+      .reduce((cookies, cookie) => {
+        const [, name, value] = cookie.match(/(\w+)=(.*)/)
+        cookies[name] = decodeURI(value)
+        return cookies
+      }, {})
+  }
+  return {}
+}
+
+const setCookie = (name, value, maxAge = 0) => {
+  if (maxAge) {
+    maxAge = `; Max-Age= ${maxAge}`
+  } else {
+    maxAge = ''
+  }
+  return `${name}=${encodeURI(value)}${maxAge}; HttpOnly; SameSite=Strict`
+}
+
+module.exports = {
+  cookieNames,
+  cookies,
+  setCookie,
+  unauthorized: ['/login', '/403', '/favicon.ico'],
+  toLogin: (request, response) => {
+    const redirect = cookies(request)[cookieNames.redirect] || encodeURIComponent(request.url.substring(1))
+    response.writeHead(302, {
+      location: '/login',
+      'set-cookie': setCookie(cookieNames.redirect, redirect)
+    })
+    response.end()
+  }
+}
