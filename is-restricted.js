d73adb231101a0ed6cf134e5dee18a818f0b8075
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-13 22:56:43 -0400

    Detect site

--- is-restricted.js
+++ is-restricted.js
@@ -1,12 +1,19 @@
-const { $restricted } = require('./common')
-const allowed = ['/login', '/403', '/favicon.ico']
+const { $restricted, $site } = require('./common')
+const allowed = [
+  '/index',
+  '/login',
+  '/403',
+  '/favicon.ico'
+]
 
 module.exports = async function isRestricted (request, response) {
   if (allowed.some(prefix => request.url.startsWith(prefix))) {
     return
   }
-  const siteName = Object.keys(cfg.sites).filter(candidate => request.url.starts(`/${siteName}`))[0]
+  const siteName = Object.keys(cfg.sites)
+    .filter(candidate => request.url.startsWith(`/${candidate}/`) || request.url === `/${candidate}`)[0]
   if (siteName) {
+    request[$site] = cfg.sites[siteName]
     request[$restricted] = cfg.sites[siteName].restricted
   }
   request[$restricted] = true
