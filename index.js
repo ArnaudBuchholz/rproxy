96c041bd636d31bcfb9ee68c35efdb87f4a3d50e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-14 23:30:27 -0400

    Redirect and use referer

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 global.cfg = require('./.rproxy.json')
 const { check, serve } = require('reserve')
 const { writeFileSync } = require('fs')
-const { $restricted, $site } = require('./common')
+const { $restricted, $site, $indirect } = require('./common')
 
 function log (...args) {
   const now = new Date().toISOString()
@@ -39,18 +39,32 @@ check({
     file: 'index.html'
   }, {
     'if-match': (request, url, match) => {
-      if (!request[$restricted] || !request[$site]) {
+      if (!request[$site] || !request[$indirect]) {
         return false
       }
+      match[2] = match[1]
+      match[1] = request[$site].forward
       return match
     },
-    url: 'https://arnaudbuchholz.github.io/'
+    match: /^\/(.*)/,
+    url: '$1/$2'
+  }, {
+    'if-match': (request, url, match) => {
+      if (!request[$site]) {
+        return false
+      }
+      match[1] = request[$site].forward
+      return match
+    },
+    match: /^\/([^\/]+)\/?(.*)?/,
+    url: '$1/$2'
   }, {
     'http-status': 403,
     file: '403.html'
   }]
 }).then(configuration => {
   serve(configuration)
+    .on('ready', () => log('READY', 0))
     .on('incoming', event => !event.internal ? log('INCMG', event.id, JSON.stringify({ ...event, id: undefined })) : 0)
     .on('redirecting', ({ id, internal, type, redirect }) => {
       if (!internal) {
@@ -63,5 +77,9 @@ check({
       }
     })
     .on('redirected', ({ id, internal, timeSpent, statusCode }) => !internal ? log('SERVE', id, JSON.stringify({ timeSpent, statusCode })) : 0)
-    .on('error', ({ id, internal, error }) => !internal ? log('ERROR', id, error.toString()) : 0)
+    .on('error', ({ id, internal, reason }) => {
+      if (!internal) {
+        log('ERROR', id, reason && reason.toString() || '')
+      }
+    })
 })
