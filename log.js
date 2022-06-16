54e81c63101e88972582dc5d6d550dc47623b42d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-15 20:11:59 -0400

    Use diff ms timestamp in log file

--- log.js
+++ log.js
@@ -3,9 +3,14 @@
 const { writeFileSync } = require('fs')
 
 module.exports = function log (...args) {
-  const now = new Date().toISOString()
-  const fileName = `logs/${now.substring(0, 10)}.csv`
-  writeFileSync(fileName, [now, ...args]
+  const now = Date.now()
+  const today = new Date(now)
+  today.setUTCHours(0)
+  today.setUTCMinutes(0)
+  today.setUTCSeconds(0)
+  today.setUTCMilliseconds(0)
+  const fileName = `logs/${new Date(now).toISOString().substring(0, 10)}.csv`
+  writeFileSync(fileName, [now - today, ...args]
     .map(arg => arg.toString())
     .map(arg => arg.match(/"|;/) ? `"${arg.replace(/"|\//g, m => `\\${m}`)}"` : arg)
     .join(';') + '\n',
