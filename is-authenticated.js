09f25556c341a6a330013399b33afb771e973c80
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-19 22:43:58 -0400

    Document authent

--- is-authenticated.js
+++ is-authenticated.js
@@ -1,16 +1,19 @@
 const jose = require('jose')
-const {
-  $restricted,
-  cookieNames,
-  cookies,
-  toLogin
-} = require('./common')
+const { read, names } = require('./cookies')
+const { $site } = require('./symbols')
+const { $requestId } = require('reserve/symbols')
+const log = require('./log')
+const toLogin = require('./to-login')
 
 module.exports = async function isAuthenticated (request, response) {
+  if (!request[$site].restricted) {
+    return
+  }
   try {
-    const token = cookies(request)[cookieNames.jwt]
+    const token = read(request)[names.jwt]
     const { payload } = await jose.jwtVerify(token, Buffer.from(cfg.jwt.secret))
     request.jwt = payload
+    log('AUTHN', request[$requestId], payload.user)
   } catch (e) {
     toLogin(request, response)
   }
