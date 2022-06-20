dab16ce6974001f8588453358514f30c91cd67c1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-19 22:43:38 -0400

    Identify site

--- identify-site.js
+++ identify-site.js
@@ -1,4 +1,8 @@
-const { $restricted, $site, $siteName, $indirect } = require('./common')
+const { $site, $referer } = require('./symbols')
+const { $requestId } = require('reserve/symbols')
+const { names, read } = require('./cookies')
+const log = require('./log')
+
 const allowed = [
   '/index',
   '/login',
@@ -6,33 +10,49 @@ const allowed = [
   '/favicon.ico'
 ]
 
+const defaultSite = {
+  restricted: false,
+  forward: '/'
+}
+
 const checkSite = (request, url = request.url) => {
   const siteName = Object.keys(cfg.sites)
     .filter(candidate => url.startsWith(`/${candidate}/`) || url === `/${candidate}`)[0]
   if (siteName) {
-    request[$siteName] = siteName
     request[$site] = cfg.sites[siteName]
-    request[$restricted] = cfg.sites[siteName].restricted
     return true
   }
   return false
 }
 
-module.exports = async function isRestricted (request, response) {
+function resolve (request) {
   if (allowed.some(prefix => request.url.startsWith(prefix))) {
+    request[$site] = defaultSite
     return
   }
   if (checkSite(request)) {
+    return
+  }
+  const cookies = read(request)
+  const siteName = cookies[names.site]
+  if (siteName) {
+    request[$site] = cfg.sites[siteName] || defaultSite
     return
   }
   if (request.headers.referer) {
     try {
       const referer = request.headers.referer.match(/https?:\/\/[^/]*(\/.*)/)[1]
       if (checkSite(request, referer)) {
-        request[$indirect] = true
+        request[$referer] = true
       }
     } catch (e) {
       // ignore
     }
   }
+}
+
+module.exports = async function identifySite (request) {
+  resolve(request)
+  const site = request[$site]
+  log('IDSIT', request[$requestId], site[$name] || '(default)', site.restricted)
 }
