d6c6382f70356a30782c0b008b7c28fb172159bd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-06-21 07:41:24 -0400

    Preprocess configuration (hostname)

--- configuration.js
+++ configuration.js
@@ -1,9 +1,16 @@
 'use strict'
 
-const { $name } = require('./symbols')
+const { $name, $forward, $hostname } = require('./symbols')
+const HOST_REGEXP = /https?:\/\/([^/:]*)/
 
 global.cfg = require('./.rproxy.json')
 
 Object.keys(cfg.sites).forEach(name => {
-  cfg.sites[name][$name] = name
+  const site = cfg.sites[name]
+  site[$name] = name
+  if (site.forward.startsWith('http')) {
+    site[$hostname] = site.forward.match(HOST_REGEXP)[1] 
+  } else {
+    site[$forward] = site.forward
+  }
 })
